4e713ba994c3ffcfd297c5739ea39c71d6d28c44
Adrien Gregorj <adrien.gregorj@gmail.com>
2022-10-08 21:11:11 +0900

    add run

--- app.py
+++ app.py
@@ -48,3 +48,7 @@ def check_answer():
             correction[rank] = 1
     response = {"score": score, "correction": correction, "ranking": daily["choices"]}
     return response
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=9007)
